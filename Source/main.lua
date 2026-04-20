252f515c6206c741ad5e56c7cb929eab425e5b76
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-04-20 15:52:18 -0300

    Hitboxes Jugador y Enemigo

--- Source/main.lua
+++ Source/main.lua
@@ -2,11 +2,16 @@
 -- Imagenes
 ImgArgentino = nil
 ImgDracula = nil
--- Posiciones
+-- Caja Jugador
 Jugador_x = 100
 Jugador_y = 400
+Jugador_alto  = 125
+Jugador_ancho = 125
+-- Caja Enemigo
 Enemigo_x = 400
 Enemigo_y = 400
+Enemigo_alto  = 125
+Enemigo_ancho = 125
 -- =================== INICIALIZACION ===================
 function love.load()
     ImgArgentino = love.graphics.newImage("Argentino.png")
@@ -32,6 +37,12 @@ function love.update(dt)
 end
 -- =================== RENDERIZADO ===================
 function love.draw()
+     -- Depurar
+    love.graphics.setColor(1,0, 0)
+    love.graphics.rectangle("line", Enemigo_x, Enemigo_y, Enemigo_ancho, Enemigo_alto)
+    love.graphics.rectangle("line", Jugador_x, Jugador_y, Jugador_ancho, Jugador_alto)
+    
+    love.graphics.setColor(1,1, 1)
     love.graphics.draw(ImgDracula, Enemigo_x,Enemigo_y)
     love.graphics.draw(ImgArgentino, Jugador_x,Jugador_y)
 end
